382a31abd9d6af750322fdfd5f5711cf32774409
mailsonbc <mailsonbc@gmail.com>
2022-07-10 16:51:37 -0300

    Criado o0 controller da classe contato

--- cooperoleo.inicio/cooperoleo/Models/CondominioModel.cs
+++ cooperoleo.inicio/cooperoleo/Models/CondominioModel.cs
@@ -3,6 +3,7 @@
     public class CondominioModel
     {
         public int CondominioId { get; set; }
+        //public ContatoModel Contato { get; set; }
         public String Contato { get; set; }
         public String Endereco { get; set; }
         public String NomeCondominio { get; set; }
--- cooperoleo.inicio/cooperoleo/Models/ContatoModel.cs
+++ cooperoleo.inicio/cooperoleo/Models/ContatoModel.cs
@@ -6,7 +6,10 @@
         public String Nome { get; set; }
         public String Cpf { get; set; }
         public String Telefone { get; set; }
-        public CondominioModel Endereco { get; set; }
+        //public CondominioModel Endereco { get; set; }
+        public String Endereco { get; set; }
+        //public CondominioModel Condominio { get; set; }
+        public String NomeCondominio { get; set; }
         public Boolean Ativo { get; set; }
     }
 }
